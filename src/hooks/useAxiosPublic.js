6e3cad874f7e3d0691847982d63cedbcad76378c
Morsed <mdmorsedalam324@gmail.com>
2024-04-02 14:12:51 +0600

    update axios base url

--- src/hooks/useAxiosPublic.js
+++ src/hooks/useAxiosPublic.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:1212/api",
+  baseURL: "https://heliverse-users-server-theta.vercel.app/api",
 });
-
+// http://localhost:1212/api
+// https://heliverse-users-server-theta.vercel.app/api
 const useAxiosPublic = () => {
   return axiosPublic;
 };
